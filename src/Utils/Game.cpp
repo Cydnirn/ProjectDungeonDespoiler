28bf7a85af2bbf956919fa04e10d43c3766aad48
Seno Aji <senoaji0003@gmail.com>
2024-11-01 00:09:47 +0700

    feat: add multi window alpha implementation

--- src/Utils/Game.cpp
+++ src/Utils/Game.cpp
@@ -9,19 +9,33 @@
 #include <string>
 #include <unistd.h>
 
-WINDOW* window;
-
 namespace ProjectDungeonDespoiler {
+    WINDOW* main_window;
+    WINDOW* game_window;
+    Game::rect game_area = {{0, 0}, {0, 0}};
+    Game::rect screen_area = {{0, 0}, {0, 0}};
+    void Game::drawBox(WINDOW* window, int pos_y, int pos_x, bool refresh)
+    {
+        if(refresh) werase(main_window);
+        wattron(window, A_BOLD);
+        box(window, pos_y, pos_x);
+        wattroff(window, A_BOLD);
+    }
+
+    void Game::enableKeyBlockInput(WINDOW* window)
+    {
+        keypad(window, true);
+        nodelay(window, false);
+    }
+
+
     int Game::init()
     {
-        window = initscr();
+        main_window = initscr();
         cbreak();
         noecho();
         clear();
         refresh();
-
-        keypad(window, true);
-        nodelay(window, true);
         curs_set(0); //Make cursor invisible
         if(!has_colors())
         {
@@ -31,9 +45,16 @@ namespace ProjectDungeonDespoiler {
         }
 
         start_color();
-        attron(A_BOLD);
-        box(window, 0, 0);
-        attroff(A_BOLD);
+
+        int infopanel_height = 4;
+        screen_area = {{0, 0}, {80, 24}};
+        game_area = {{0, 0}, {static_cast<int_fast8_t>(screen_area.width() - 2), static_cast<int_fast8_t>(screen_area.height() - infopanel_height - 4)}};
+        game_window = newwin(static_cast<int>(screen_area.height() - infopanel_height - 2), static_cast<int>(screen_area.width() - 2),
+                             static_cast<int>(screen_area.top() + 1), static_cast<int>(screen_area.left() + 1));
+        main_window = newwin(static_cast<int>(screen_area.height()), static_cast<int>(screen_area.width()), 0, 0);
+
+        enableKeyBlockInput(game_window);
+        enableKeyBlockInput(main_window);
 
         return 0;
     }
@@ -41,40 +62,55 @@ namespace ProjectDungeonDespoiler {
     void Game::run()
     {
         player.display_char = *"0";
-        player.position = {10, 5};
-        refresh();
+        player.position = {1, 1};
         /* Main game loop */
         bool exit_req = false;
+        drawBox(main_window, 0, 0, false);
+        wmove(main_window, static_cast<int>(game_area.bottom() + 3), 1);
+        whline(main_window, '-', static_cast<int>(screen_area.width() - 2));
+
+        wrefresh(main_window);
+        mvwaddch(game_window, player.position.y, player.position.x, player.display_char);
+        wrefresh(game_window);
 
         while(true)
         {
-            mvaddch(player.position.y, player.position.x, ' ');
-            switch (int in_char = wgetch(window))
+            werase(game_window);
+
+            mvwaddch(game_window, player.position.y, player.position.x, ' ');
+            switch (tolower(wgetch(main_window)))
             {
                 case 'q':
                     exit_req = true;
                     break;
                 case KEY_UP:
                 case 'w':
-                    player.position.y -=1;
+                    if(player.position.y > game_area.top())
+                        player.position.y -= 1;
                     break;
                 case KEY_DOWN:
-                case 's':
-                    player.position.y += 1;
+            case 's':
+                    if(player.position.y < game_area.bottom())
+                        player.position.y += 1;
                     break;
                 case KEY_LEFT:
                 case 'a':
-                    player.position.x -=1;
-                    break;
-                case KEY_RIGHT:
+                    if(player.position.x > game_area.left() + 1)
+                        player.position.x -=1;
+                        break;
+                    case KEY_RIGHT:
                 case 'd':
-                    player.position.x += 1;
-                    break;
+                    if(player.position.x < game_area.right() - 2)
+                        player.position.x += 1;
+                        break;
                 default:
                     break;
             }
-            mvaddch(player.position.y, player.position.x, player.display_char);
-            refresh();
+            wattron(game_window, A_BOLD);
+            mvwaddch(game_window,player.position.y, player.position.x, player.display_char);
+            wattroff(game_window, A_BOLD);
+            wrefresh(main_window);
+            wrefresh(game_window);
             if (exit_req) break;
             usleep(10000);
         };
--- src/Utils/Game.h
+++ src/Utils/Game.h
@@ -5,6 +5,7 @@
 #ifndef GAME_H
 #define GAME_H
 #include <cstdint>
+#include <ncurses.h>
 
 namespace ProjectDungeonDespoiler {
 
@@ -14,6 +15,11 @@ public:
     static int init();
     void run();
     static void close();
+
+private:
+    static void drawBox(WINDOW* window, int pos_y = 0, int pos_x = 0, bool refresh = true);
+    static void enableKeyBlockInput(WINDOW* window);
+
 private:
     typedef struct
     {
@@ -27,9 +33,28 @@ private:
         int_fast8_t y;
     } vec2i;
 
+public:
+    typedef struct
+    {
+        vec2i offset;
+        vec2i bounds;
+        [[nodiscard]] uint_fast16_t top() const {return  offset.y;}
+        [[nodiscard]] uint_fast16_t bottom() const {return offset.y + bounds.y;}
+        [[nodiscard]] uint_fast16_t left() const {return offset.x;}
+        [[nodiscard]] uint_fast16_t right() const {return offset.x + bounds.x;}
+
+        [[nodiscard]] uint_fast16_t width() const {return bounds.x;}
+        [[nodiscard]] uint_fast16_t height() const {return bounds.y;}
+
+        [[nodiscard]] bool contains (const vec2i a) const{return (a.x >= offset.x && a.x < right()) &&
+            (a.y >= offset.y && a.y < bottom());}
+    } rect;
+
+private:
     struct
     {
         vec2i position;
+        rect bounds;
         char display_char;
     } player = {};
 };
