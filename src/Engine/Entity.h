d60f88fa5f238ec912e583bb922c3ccacc57a3d9
Seno Aji <senoaji0003@gmail.com>
2024-12-07 18:08:17 +0700

    feat: update Entity

--- src/Engine/Entity.h
+++ src/Engine/Entity.h
@@ -1,45 +1,23 @@
 //
-// Created by rei on 12/4/24.
+// Created by rei on 12/6/24.
 //
-#pragma once
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
 
-#include "Math.h"
-#ifndef ENTITY_H
-#define ENTITY_H
+#ifndef PROJECTDUNGEONDESPOILER_ENTITY_H
+#define PROJECTDUNGEONDESPOILER_ENTITY_H
+#include <SDL.h>
 
 namespace DespoilerEngine {
 
 class Entity {
 public:
-  Entity(Vector2f p_pos, SDL_Texture* p_tex);
-  Vector2f& getPos()
-  {
-    return pos;
-  }
-  float getAngle()
-  {
-    return angle;
-  }
-  Vector2f getScale()
-  {
-    return scale;
-  }
-  void setPos(float x, float y);
-  void setScale(float w, float h);
-  void setAngle(float angle);
-  SDL_Texture *getTex() const;
-  static void update(double deltaTime);
-  SDL_Rect getCurrentFrame() const;
-private:
-  Vector2f pos;
-  float angle = 0;
-  Vector2f scale = Vector2f(1, 1);
-  SDL_Rect currentFrame{};
-  SDL_Texture* tex;
+  virtual ~Entity() = default;
+  Entity(SDL_Texture* texture, SDL_Rect src, SDL_Rect dst)
+      : e_texture(texture), e_src(src), e_dst(dst) {}
+  virtual void handleEvent(SDL_Event &e) = 0;
+  SDL_Texture* e_texture;
+  SDL_Rect e_src, e_dst;
 };
 
-} // DespoilerEngine
+} // namespace DespoilerEngine
 
-#endif //ENTITY_H
+#endif // PROJECTDUNGEONDESPOILER_ENTITY_H
