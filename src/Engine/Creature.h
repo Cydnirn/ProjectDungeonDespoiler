03ac7d8276d8ab331d2cffc254744981807c397e
Seno Aji <senoaji0003@gmail.com>
2024-12-16 22:00:17 +0700

    feat: Add baseDamage and base damage modifier to player

--- src/Engine/Creature.h
+++ src/Engine/Creature.h
@@ -11,19 +11,23 @@ class Creature {
     std::string description;
     std::string name;
     Stats stats{0,0,0,0};
+    int baseDamage = 10 * stats.strength;
+    int baseCritical = stats.agility;
     int health = 10 * stats.vigor;
     int mana = 10 * stats.intelligence;
     int xp = 100;
     int level = xp / 100;
 public:
     ~Creature() = default;
-    Creature(std::string  name, std::string  desc, Stats  stats, int xp = 100);
+    Creature(std::string  name, std::string  desc, Stats  stats, int xp = 100, int baseDmg = 10);
 
     // Load a creature from a JSON file
     static Creature fromJsonFile(const std::string& filepath);
 
     // Getters
     [[nodiscard]] const std::string& getName() const { return name; };
+    [[nodiscard]] const int& getBaseDamage() const { return baseDamage; };
+    [[nodiscard]] const int& getBaseCritical() const { return baseCritical; };
     [[nodiscard]] const Stats &getStats() const { return stats; };
     [[nodiscard]] const std::string& getDescription() const { return description; };
     [[nodiscard]] const int& getHealth() const { return health; };
@@ -31,6 +35,7 @@ public:
     [[nodiscard]] const int& getXP() const { return xp; };
     [[nodiscard]] const int& getLevel() const { return level; };
     // Setters
+    void setBaseDamage(int baseDmg) { this->baseDamage = baseDmg; }
     void setHealth(int healthInput) { this->health = healthInput; }
     void setMana(int manaInput) { this->mana = manaInput; }
     void setXP(int xpInput) { this->xp = xpInput; }
